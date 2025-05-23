985433f5805c133f18d5fb8d03492df07ff48929
Blacksanta10 <moutonwebb29@gmail.com>
2025-05-22 23:29:51 -0400

    Tried to play around with the front page. I wasn't able to properly display the image. This is all from the web view at the moment.

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
+import { SafeAreaView, SafeAreaProvider} from "react-native-safe-area-context";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+  return ( 
+    <SafeAreaProvider>
+
+      <SafeAreaView style={{height: 100, flexDirection: 'row'}}>
+        <View style={{backgroundColor: 'blue', flex: 0.2}} />
+        <View style={{backgroundColor: 'red', flex: 0.4}} />
+        <Text>Hello World!</Text>
+      </SafeAreaView>
+
+    </SafeAreaProvider>
   );
 }
+
+ export function DisplayImage () {
+   return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Image
+           style= {{width: 200, height: 200}} 
+           source={require('../assets/images/Chiyo.png')}
+           />
+      </SafeAreaView>
+    </SafeAreaProvider>
+   )
+
+
+ }
